28713a231daae903399d3b6410db4d66d62c3df0
Kiran25-dev <kumarkiran7100@gmail.com>
2026-06-21 20:04:11 +0530

    Add files via upload

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,14 @@
+package com.example;
+
+
+public class App 
+{
+	public static int add(int a,int b){
+		return a+b;
+}
+
+    	public static void main( String[] args )
+    {
+        System.out.println( "sum="+ add(10,20) );
+    }
+}
